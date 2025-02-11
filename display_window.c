b230a876be6ceadddeb36bd04b92d64dd827ac42
Mouncif Fahmi <mfahmi@c2r8p6.1337.ma>
2025-02-11 12:02:23 +0100

    s

--- display_window.c
+++ display_window.c
@@ -6,7 +6,7 @@
 /*   By: mfahmi <mfahmi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:09:50 by mfahmi            #+#    #+#             */
-/*   Updated: 2025/02/10 19:15:36 by mfahmi           ###   ########.fr       */
+/*   Updated: 2025/02/11 12:01:49 by mfahmi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,44 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void display_window(char **map)
+
+void display_map(void *img_floor, void *img_wall, void *img_coin, void *img_exit, void *img_player)
 {
-	void	*img;
-	void	*mlx;
-    void    *mlx_win;
     int i;
     int j;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080);
-    while(map[i])
+    void *mlx;
+    void *new_win
+    mlx = mlx_init();
+    new_win = mlx_new_window(mlx, , ,"so_long");
+    i = 0;
+    while (map[i])
     {
-        while(map[i][j])
+        j = 0;
+        while (map[i][j])
         {
-            if (map[i][j] == '0')
-            else if (map[i][j] == '1')
-            else if (map[i][j] == 'P')
-            else if (map[i][j] == 'E')
-            else if (map[i][j] == 'C')
-            j++;
+            if(map[i][j] == '0')
+                mlx_put_img_to_window(mlx, new_win,)
+            else if(map[i][j] == '1')
+                mlx_put
+            else if(map[i][j] == 'C')
+            else if(map[i][j] == 'E')
+            else if(map[i][j] == 'P')
+        
+            j++:
         }
         i++;
     }
-    mlx_loop(mlx);
+}
+void display_window(char **map)
+{
+	void	*img_floor, *img_wall, *img_coin, *img_exit, *img_player;
+	void	*mlx;
+    void    *mlx_win;
+
+        
+                img_floor = mlx_xpm_file_to_image(mlx, FLOOR_XPM);
+                img_wall = mlx_xpm_file_to_image(mlx, WALL_XPM);
+                img_player = mlx_xpm_file_to_image(mlx, PLAYER_LEFT_XPM);
+                img_exit = mlx_xpm_file_to_image(mlx, EXIT_CLOSED_XPM);
+                img_coin = mlx_xpm_file_to_image(mlx, COINS_XPM);
 }
